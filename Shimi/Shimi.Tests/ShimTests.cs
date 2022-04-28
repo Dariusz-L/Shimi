0312a9547932fe063e0eb49c776e86e8ae9aba16
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-04-29 01:58:10 +0200

    Add tests

--- Shimi/Shimi.Tests/ShimTests.cs
+++ Shimi/Shimi.Tests/ShimTests.cs
@@ -7,16 +7,84 @@ namespace Shimi.Tests
     public class ShimTests
     {
         [Test]
-        public void GivenNoShimsBefore_WhenModifiedDirectoryGetFiles_ThenCorrect()
+        public void ShouldReplaceExtensionMethodWithFuncArg_Lambda()
+        {
+            var x = new X();
+
+            Shim.ResultOf(() => x.ExtensionMethodWithFuncArg(P.Any<Func<int, string>>())).To(10);
+            Assert.AreEqual(10, x.ExtensionMethodWithFuncArg(x => string.Empty));
+
+            Shim.ResultOf(() => XExtensions.ExtensionMethodWithFuncArg(P.Any<X>(), P.Any<Func<int, string>>())).To(15);
+            Assert.AreEqual(15, XExtensions.ExtensionMethodWithFuncArg(x, x => string.Empty));
+        }
+
+        //[Test]
+        public void ShouldReplaceExtensionMethodWithFuncArg_Any()
+        {
+            var x = new X();
+
+            Shim.ResultOf(() => x.ExtensionMethodWithFuncArg(P.Any<Func<int, string>>())).To(10);
+            var a = P.Any<Func<int, string>>();
+            try
+            {
+                var res = x.ExtensionMethodWithFuncArg(a);
+                Assert.AreEqual(10, res);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Shim.ResultOf(() => XExtensions.ExtensionMethodWithFuncArg(P.Any<X>(), P.Any<Func<int, string>>())).To(15);
+            Assert.AreEqual(15, XExtensions.ExtensionMethodWithFuncArg(x, X.FuncArg));
+        }
+
+        [Test]
+        public void ShouldReplaceExtensionMethodWithFuncArg_StaticMethod()
+        {
+            var x = new X();
+
+            Shim.ResultOf(() => x.ExtensionMethodWithFuncArg(P.Any<Func<int, string>>())).To(10);
+            Assert.AreEqual(10, x.ExtensionMethodWithFuncArg(X.FuncArg));
+
+            Shim.ResultOf(() => XExtensions.ExtensionMethodWithFuncArg(P.Any<X>(), P.Any<Func<int, string>>())).To(15);
+            Assert.AreEqual(15, XExtensions.ExtensionMethodWithFuncArg(x, X.FuncArg));
+        }
+
+        [Test]
+        public void ShouldReplaceExtensionMethodWithFuncArg_Null()
+        {
+            var x = new X();
+
+            Shim.ResultOf(() => x.ExtensionMethodWithFuncArg(P.Any<Func<int, string>>())).To(10);
+            Assert.AreEqual(10, x.ExtensionMethodWithFuncArg(null));
+
+            Shim.ResultOf(() => XExtensions.ExtensionMethodWithFuncArg(P.Any<X>(), P.Any<Func<int, string>>())).To(15);
+            Assert.AreEqual(15, XExtensions.ExtensionMethodWithFuncArg(x, null));
+        }
+
+        [Test]
+        public void ShouldReplaceExtensionMethod()
+        {
+            var x = new X();
+
+            Shim.ResultOf(() => x.ExtensionMethod()).To(10);
+            Assert.AreEqual(10, x.ExtensionMethod());
+
+            Shim.ResultOf(() => XExtensions.ExtensionMethod(P.Any<X>())).To(15);
+            Assert.AreEqual(15, XExtensions.ExtensionMethod(x));
+        }
+
+        [Test]
+        public void ShouldReplaceDirectoryGetFiles()
         {
             Shim.ResultOf(() => Directory.GetFiles(P.Any<string>())).To(new string[] { "X" }, out var shim);
             Assert.IsTrue(Directory.GetFiles("Random string")[0] == "X");
             Shim.Clear(shim);
-            Assert.Throws<DirectoryNotFoundException>(() => Directory.GetFiles("Random string"));
+            Assert.Throws<DirectoryNotFoundException>(() => Directory.GetFiles("Random path"));
         }
 
         [Test]
-        public void GivenNoShimsBefore_WhenModifiedDateTimeNow_ThenCorrect()
+        public void ShouldReplaceDateTimeNow()
         {
             Shim.ResultOf(() => DateTime.Now).To(DateTime.MinValue, out var shim);
             Assert.AreEqual(DateTime.MinValue, DateTime.Now);
@@ -25,57 +93,51 @@ namespace Shimi.Tests
         }
 
         [Test]
-        public void GivenNoShimsBefore_WhenModifiedInstanceMethodWithArg_ThenCorrect()
+        public void ShouldReplaceInstanceMethodWithArg()
         {
             var x = new X();
 
-            Assert.AreEqual(5, x.InstanceMethodWithArg(5));
             Shim.ResultOf(() => x.InstanceMethodWithArg(5)).To(10);
             Assert.AreEqual(10, x.InstanceMethodWithArg(5));
         }
 
         [Test]
-        public void GivenNoShimsBefore_WhenModifiedInstanceMethod_ThenCorrect()
+        public void ShouldReplaceInstanceMethod()
         {
             var x = new X();
 
-            Assert.AreEqual(0, x.InstanceMethod());
             Shim.ResultOf(() => x.InstanceMethod()).To(10);
             Assert.AreEqual(10, x.InstanceMethod());
         }
 
         [Test]
-        public void GivenNoShimsBefore_WhenModifiedInstanceProperty_ThenCorrect()
+        public void ShouldReplaceInstanceProperty()
         {
             var x = new X();
 
-            Assert.AreEqual(0, x.InstanceProperty);
             Shim.ResultOf(() => x.InstanceProperty).To(10);
             Assert.AreEqual(10, x.InstanceProperty);
         }
 
         [Test]
-        public void GivenNoShimsBefore_WhenModifiedStaticProperty_ThenCorrect()
+        public void ShouldReplaceStaticProperty()
         {
-            Assert.AreEqual(0, X.StaticProperty);
             Shim.ResultOf(() => X.StaticProperty).To(10, out var shim);
             Assert.AreEqual(10, X.StaticProperty);
             Shim.Clear(shim);
         }
 
         [Test]
-        public void GivenNoShimsBefore_WhenModifiedStaticMethod_ThenCorrect()
+        public void ShouldReplaceStaticMethod()
         {
-            Assert.AreEqual(0, X.StaticMethod());
             Shim.ResultOf(() => X.StaticMethod()).To(7, out var shim);
             Assert.AreEqual(7, X.StaticMethod());
             Shim.Clear(shim);
         }
 
         [Test]
-        public void GivenNoShimsBefore_WhenCreatedShimAndCleared_ThenModifiedMethodReset()
+        public void ShouldClearStaticMethod()
         {
-            Assert.AreEqual(0, X.StaticMethod());
             Shim.ResultOf(() => X.StaticMethod()).To(7, out var shim);
             Assert.AreEqual(7, X.StaticMethod());
             Shim.Clear(shim);
@@ -88,9 +150,16 @@ namespace Shimi.Tests
         public static readonly int StaticField = 0;
         public static int StaticProperty => 0;
         public static int StaticMethod() => 0;
+        public static string FuncArg(int x) => string.Empty;
 
         public int InstanceProperty => 0;
         public int InstanceMethod() => 0;
         public int InstanceMethodWithArg(int x) => x;
+    }
+
+    public static class XExtensions
+    {
+        public static int ExtensionMethod(this X x) => 0;
+        public static int ExtensionMethodWithFuncArg(this X x, Func<int, string> function) => 0;
     }
 }
