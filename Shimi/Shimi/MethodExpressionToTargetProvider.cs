b328c20f0120f61e5c07e48976435bf0acd459ea
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-04-30 15:05:31 +0200

    Make instance generic methods work, although to refactor

--- Shimi/Shimi/MethodExpressionToTargetProvider.cs
+++ Shimi/Shimi/MethodExpressionToTargetProvider.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq.Expressions;
+﻿using HarmonyLib;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace Shimi
 {
@@ -20,8 +23,50 @@ namespace Shimi
         public static MethodInfo GetMethodFrom(MethodCallExpression expression)
         {
             var target = GetTargetFrom(expression);
-            if (target == null)
-                return expression.Method;
+            //if (target == null)
+            {
+                var expMethod = expression.Method;
+                var declaringType = target?.GetType() ?? expMethod.DeclaringType;
+
+                //return expMethod;
+                var parameterTypes = expMethod.GetParameters().ToArray();
+                var genericTypes = expMethod.GetGenericArguments();
+                var methods = declaringType.GetMethods();
+                var methodsOfName = methods.Where(m => m.Name == expMethod.Name).ToArray();
+                var methodsWithSameMetadata = methodsOfName.Where(
+                    m =>
+                    {
+                        var mParams = m.GetParameters();
+                        if (mParams.Length != parameterTypes.Length)
+                            return false;
+
+                        var sameParams = mParams.Zip(parameterTypes, (x, y) => new { x, y })
+                            .All(p =>
+                            {
+                                var isSameType = p.x.ParameterType.Equals(p.y.ParameterType);
+                                var isSameOut = p.x.IsOut.Equals(p.y.IsOut);
+                                var isSameIn = p.x.IsIn.Equals(p.y.IsIn);
+                                var isSameIsRetval = p.x.IsRetval.Equals(p.y.IsRetval);
+                                var isSameName = p.x.Name.Equals(p.y.Name);
+
+                                return isSameType && isSameOut && isSameIn && isSameIsRetval && isSameName;
+                            });
+
+                        return sameParams;
+                    })
+                .ToArray();
+
+                var method = methodsWithSameMetadata.FirstOrDefault();
+                if (method == null)
+                {
+                    var m = methodsOfName.Select(m => m.MakeGenericMethod(genericTypes)).Where(m => m != null).FirstOrDefault();
+                    return m;
+                }
+                if (method.IsGenericMethod)
+                    return method.MakeGenericMethod(genericTypes);
+
+                return method;
+            }
 
             var targetType = target.GetType();
             return targetType.GetMethod(expression.Method.Name);
